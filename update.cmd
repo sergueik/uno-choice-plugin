5e31b7a6b2805d417311a49fbe029243318befa7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-01-23 09:36:20 -0500

    listed more plugins in use

--- update.cmd
+++ update.cmd
@@ -8,16 +8,13 @@ set TITLE=Windows Slaves Plugin
 set HPI=windows-slaves.hpi
 call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
 
-
 set TITLE=Token Macro Plugin
 set HPI=token-macro.hpi
 call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
 
-
 set TITLE=Scriptler Plugin
 set HPI=scriptler.hpi
 call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
-
 
 set TITLE=SCM API Plugin
 set HPI=scm-api.hpi
@@ -109,61 +106,42 @@ set TITLE=Credentials Plugin
 set HPI=credentials.hpi
 call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
 
+set TITLE=JQuery
+set HPI=jquery.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+set TITLE=Node and Label parameter plugin
+set HPI=nodelabelparameter.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+
+set TITLE=Mask Passwords Plugin
+set HPI=mask-passwords.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+set TITLE=Promoted Builds ^(Simple^)
+set HPI=promoted-builds-simple.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+set TITLE=Hudson PowerShell plugin
+set HPI=powershell.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+set TITLE=Build User Vars Plugin
+set HPI=build-user-vars-plugin.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+set TITLE=Copy Artifact Plugin
+set HPI=copyartifact.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
+
+set TITLE=Monitoring external jobs
+set HPI=external-monitor-job.hpi
+call wget-1.10.2.exe --no-check-certificate %BASE_URL%/%HPI%
+
 goto :EOF
 
-Ant Plugin
-This plugin adds Apache Ant support to Jenkins.
-1.2			
-	
 
-	
-Credentials Plugin
-This plugin allows you to store credentials in Jenkins.
-1.18			
-	
-CVS Plugin
-Integrates Jenkins with CVS version control system using a modified version of the Netbeans cvsclient.
-2.11			
-	
-	
-Javadoc Plugin
-This plugin adds Javadoc support to Jenkins.
-1.1			
-	
-LDAP Plugin
-Security realm based on LDAP authentication.
-1.6			
-	
-Mailer
-This plugin allows you to configure email notifications. This is a break-out of the original core based email component.
-1.11			
-			
-Maven Project Plugin
-Jenkins plugin for building Maven 2/3 jobs via a special project type.
-2.7.1			
-	
-OWASP Markup Formatter Plugin
-Uses the OWASP Java HTML Sanitizer to allow safe-seeming HTML markup to be entered in project descriptions and the like.
-1.1			
-	
-PAM Authentication Plugin
-Adds Unix Pluggable Authentication Module (PAM) support to Jenkins.
-1.1			
-
-	
-	
-Subversion Plugin
-This plugin adds the Subversion support (via SVNKit) to Jenkins.
-1.54			
-	
-
-	
-Translation Assistance Plugin
-This plugin adds an additional dialog box in every page, which enables people to contribute localizations for the messages they are seeing in the current page.
-1.10			
-	
 Uno-Choice Plug-in
-This plug-in provides additional parameter types for jobs, that allow you to cascade changes and render images or other HTML elements instead of the traditional parameter.
-0.24-SNAPSHOT (private-01/20/2015 19:37-sergueik)			
-
 
